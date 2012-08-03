92b41f6906164df4eb55512ac5efe72fe377c71c
DallonF <dallonfdev@gmail.com>
2012-08-03 11:19:15 -0700

    Added grunt

---
+++ grunt.js
@@ -0,0 +1,18 @@
+require('shelljs/global');
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+      less: {}
+    , watch: {
+      less: {
+          files: ['./less/*.less', './bootstrap/less/*.less']
+        , tasks: 'less'
+      }
+    }
+  });
+
+
+  grunt.registerTask('less', function() {
+    exec('lessc ./less/app.less', {silent: true}).output.to('./public/css/app.css');
+  });
+};
